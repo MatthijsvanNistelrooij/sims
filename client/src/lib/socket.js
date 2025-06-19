3c5bfddf6113bf5d75356d30b474ec8f4a5a612d
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-19 03:19:18 +0200

    update origin url

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client"
 import { atom } from "jotai"
 
 // export const socket = io("http://localhost:3001/")
-export const socket = io("https://sims-es13.onrender.com/")
+const socketUrl = import.meta.env.VITE_SOCKET_URL
 
+export const socket = io(socketUrl)
 export const charactersAtom = atom([])
